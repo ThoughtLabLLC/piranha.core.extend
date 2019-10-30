4211538188b60a673e7ffeb555b238ad715bcb8d
Ali Zhao <aliz@thoughtlab.com>
2019-10-30 12:04:33 -0600

    started new field models

---
+++ src/piranha.core.extend/Fields/HeadingField.cs
@@ -0,0 +1,44 @@
+﻿using Piranha.Extend;
+using Piranha.Extend.Fields;
+
+namespace Thoughtlab.Piranha.Extend.Fields
+{
+	[FieldType(Name = "Heading", Shorthand = "Heading", Component = "heading-field")]
+	public class HeadingField : SimpleField<string>
+	{
+		/// <summary>
+		/// Gets/sets the heading level.
+		/// </summary>
+		/// <returns></returns>
+		public HeadingFieldLevel HeadingLevel { get; set; }
+
+		/// <summary>
+		/// Implicit operator for converting a string to a field.
+		/// </summary>
+		/// <param name="str">The string value</param>
+		public static implicit operator HeadingField(string str)
+		{
+			return new HeadingField { Value = str };
+		}
+
+		/// <summary>
+		/// Implicitly converts the Heading field to a string.
+		/// </summary>
+		/// <param name="field">The field</param>
+		public static implicit operator string(HeadingField field)
+		{
+			return field.Value;
+		}
+	}
+
+	public enum HeadingFieldLevel
+	{
+		None = 0,
+		H1 = 1,
+		H2 = 2,
+		H3 = 3,
+		H4 = 4,
+		H5 = 5,
+		H6 = 6
+	}
+}
---
+++ src/piranha.core.extend/Fields/LinkField.cs
@@ -0,0 +1,18 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thoughtlab.Piranha.Extend.Fields
+{
+	public class LinkField : IField
+	{
+
+		public string Url { get; set; }
+
+		public string GetTitle()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
