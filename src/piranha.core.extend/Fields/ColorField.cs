5bfbcc8a3b7a78d22d500d7ef86b0f4136fc8336
Ali Zhao <aliz@thoughtlab.com>
2019-11-13 10:32:50 -0700

    start color field model

---
+++ src/piranha.core.extend/Fields/ColorField.cs
@@ -0,0 +1,13 @@
+﻿using Piranha.Extend;
+using Piranha.Extend.Fields;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thoughtlab.Piranha.Extend.Fields
+{
+	[FieldType(Name = "Color Picker", Shorthand = "Color", Component = "color-field")]
+	public class ColorField : SimpleField<string>
+	{
+	}
+}
